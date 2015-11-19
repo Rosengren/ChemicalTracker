bcf1c8ce8b467645f5021eb6a088902f0d158e3a
Kevin Rosengren <kev.rosengren@gmail.com>
2015-11-18 19:27:28 -0500

    added basic S3 DAO

--- src/main/java/com/chemicaltracker/persistence/ImageDataAccessObject.java
+++ src/main/java/com/chemicaltracker/persistence/ImageDataAccessObject.java
@@ -1,6 +1,7 @@
-
 package com.chemicaltracker.persistence;
 
 public interface ImageDataAccessObject {
 
+    public void uploadImage(final String filePath,
+            final String filename, final String bucketName);
 }
---
+++ src/main/java/com/chemicaltracker/persistence/ImageDataAccessS3.java
@@ -0,0 +1,73 @@
+package com.chemicaltracker.persistence;
+
+import java.io.File;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import org.apache.log4j.Logger;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+
+public class ImageDataAccessS3 implements ImageDataAccessObject {
+
+    private static final Logger logger = Logger.getLogger(ImageDataAccessS3.class);
+
+    private AmazonS3Client s3client;
+
+    public ImageDataAccessS3() {
+
+        try {
+            initializeDBConnection();
+        } catch (Exception e) {
+            logger.error("Error occured while initializing DB Connection", e);
+        }
+    }
+
+    public void initializeDBConnection() throws AmazonClientException {
+
+        AWSCredentials credentials = null;
+
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException("Could not load credentials", e);
+        }
+
+        try {
+            s3client = new AmazonS3Client(credentials);
+        } catch (Exception e) {
+            throw new AmazonClientException("The provided credentials were not valid", e);
+        }
+    }
+
+    @Override
+    public void uploadImage(final String filePath,
+            final String filename, final String bucketName) {
+
+        // TODO: make sure to consider what to name the file (ex: Storage is called location
+        File imageFile = null;
+
+        try {
+            imageFile = new File(filePath);
+        } catch(Exception e) {
+            logger.error("Error occurred while trying to fetch file: " + filePath);
+        }
+
+        try {
+            s3client.putObject(new PutObjectRequest(bucketName, filename, imageFile));
+        } catch (AmazonServiceException ase) {
+            logger.error("The image was rejected from S3", ase);
+        } catch (AmazonClientException ace) {
+            logger.error("Error occurred while trying to put file: " + filePath +
+                    " with filename: " + filename + " into bucket: " + bucketName, ace);
+        }
+    }
+}
